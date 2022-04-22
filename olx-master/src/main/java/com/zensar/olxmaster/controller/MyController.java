943f000266bb47ffa4789720b7b0379b7abf15bd
mobeen <m.mobeen@zensar.com>
2022-04-22 12:58:48 +0530

    consumes and produces commit

---
+++ olx-master/src/main/java/com/zensar/olxmaster/controller/MyController.java
@@ -0,0 +1,46 @@
+package com.zensar.olxmaster.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.zensar.olxmaster.entity.Category;
+import com.zensar.olxmaster.entity.Status;
+
+
+@RestController
+public class MyController {
+	
+	static List<Category> categories= new ArrayList<Category>();
+	static List<Status> status=new ArrayList<Status>();
+	
+	static {
+		categories.add(new Category(1L, "Furniture"));
+		categories.add(new Category(2L, "Cars"));
+		categories.add(new Category(3L, "Mobiles"));
+		categories.add(new Category(4L, "Real Estate"));
+		categories.add(new Category(5L, "Sports"));
+	}
+	
+	static {
+		status.add(new Status(1L, "OPEN"));
+		status.add(new Status(2L, "CLOSED"));
+	}
+	
+	@GetMapping(value="/advertise/category",produces = {MediaType.APPLICATION_XML_VALUE ,MediaType.APPLICATION_JSON_VALUE})
+	public List<Category> getAllAdvertisementCategories() {
+		return categories;
+	}
+	
+	@GetMapping(value="/advertise/status",produces = {MediaType.APPLICATION_XML_VALUE ,MediaType.APPLICATION_JSON_VALUE})
+	public List<Status> getAllAdvertisementStatus(){
+		return status;
+	}
+	
+
+}
---
+++ olx-master/src/main/java/com/zensar/olxmaster/entity/Status.java
@@ -0,0 +1,14 @@
+package com.zensar.olxmaster.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Status {
+	private long id;
+	private String status;
+
+}
